5ce9d104c8179c537b1d831b67583b4456dcf15d
kpt 財家孝佑 <zaike@key-p.co.jp>
2013-08-06 10:30:06 +0900

    ds

--- GitInfoApp/src/com/example/gitinfoapp/ListActivity.java
+++ GitInfoApp/src/com/example/gitinfoapp/ListActivity.java
@@ -21,7 +21,7 @@ import com.zako.dialog.CustomProgressDialog;
 
 /**
  * ˆê——‰æ–Ê
- * @author zaike
+ * @author zaikea
  */
 public class ListActivity extends Activity implements Handler.Callback {
 
